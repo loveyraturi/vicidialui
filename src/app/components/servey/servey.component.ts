06281baab0c8eb8fca6efc599285136f99df3826
praveenraturi94 <praveenraturi3@yahoo.com>
2019-12-21 20:06:46 +0530

    changes

--- src/app/components/servey/servey.component.ts
+++ src/app/components/servey/servey.component.ts
@@ -28,29 +28,29 @@ export class ServeyComponent implements OnInit {
 
   private createForm(): void {
 
-    this.registerform = new FormGroup({
-      campaign: new FormGroup({
-        campaign_name: new FormControl('', [Validators.required]),
-        campaign_description: new FormControl('', Validators.required),
-        web_form_address: new FormControl('', Validators.required),
-        user_group: new FormControl('', Validators.required),
-        hopper_level: new FormControl('', Validators.required),
-        allow_closers: new FormControl('', Validators.required),
-        auto_dial_level: new FormControl('', Validators.required),
-        local_call_time: new FormControl('', Validators.required),
-        next_agent_call: new FormControl('', Validators.required),
-        get_call_launch: new FormControl('', Validators.required),
-        campaign_script: new FormControl('', Validators.required),
-        active: new FormControl('', Validators.required),
+    // this.registerform = new FormGroup({
+    //   campaign: new FormGroup({
+    //     campaign_name: new FormControl('', [Validators.required]),
+    //     campaign_description: new FormControl('', Validators.required),
+    //     web_form_address: new FormControl('', Validators.required),
+    //     user_group: new FormControl('', Validators.required),
+    //     hopper_level: new FormControl('', Validators.required),
+    //     allow_closers: new FormControl('', Validators.required),
+    //     auto_dial_level: new FormControl('', Validators.required),
+    //     local_call_time: new FormControl('', Validators.required),
+    //     next_agent_call: new FormControl('', Validators.required),
+    //     get_call_launch: new FormControl('', Validators.required),
+    //     campaign_script: new FormControl('', Validators.required),
+    //     active: new FormControl('', Validators.required),
 
-      }),
-      group: new FormGroup({
-        user_group: new FormControl('', Validators.required),
-        group_name: new FormControl('', Validators.required),
-        allowed_campaigns: new FormControl('', Validators.required)
+    //   }),
+    //   group: new FormGroup({
+    //     user_group: new FormControl('', Validators.required),
+    //     group_name: new FormControl('', Validators.required),
+    //     allowed_campaigns: new FormControl('', Validators.required)
 
-      })
-    });
+    //   })
+    // });
   }
 
 }
