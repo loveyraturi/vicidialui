a3f59cc9501852edc667b642f801bc03385432bb
praveenraturi94 <praveenraturi3@yahoo.com>
2019-12-26 13:48:38 +0530

    changes

--- src/app/components/reporting/reporting.component.ts
+++ src/app/components/reporting/reporting.component.ts
@@ -3,6 +3,7 @@ import * as XLSX from 'xlsx';
 import { ExportService } from 'app/services/export.service';
 import { Login } from 'app/models/login';
 import { UserService } from 'app/services/user.service';
+import { CampaingService } from 'app/services/campaing.service';
 @Component({
   selector: 'app-reporting',
   templateUrl: './reporting.component.html',
@@ -16,32 +17,55 @@ export class ReportingComponent implements OnInit {
   customers: any = [];
   public username;
   public reportData;
+  public campaings;
+  public users;
   loginInfo: Login = {
     user_name: null,
   }
+  dropdownList = [];
+  dropdownUserList=[]
+  selectedItems = [];
+  selectedUserItems = [];
+  dropdownSettings = {};
+  dropdownUserSettings = {};
   public headers;
   public valuenull = "-"
   datefrom: Date = new Date();
   dateto: Date = new Date();
   settings = {
-      bigBanner: true,
-      timePicker: true,
-      format: 'dd-MM-yyyy HH:mm:ss',
-      defaultOpen: false,
-      closeOnSelect:true
+    bigBanner: true,
+    timePicker: true,
+    format: 'dd-MM-yyyy HH:mm:ss',
+    defaultOpen: false,
+    closeOnSelect: true
   }
   public datapresent
-  constructor(private exportService: ExportService, private userService: UserService) { }
+  constructor(private campaingService: CampaingService, private exportService: ExportService, private userService: UserService) {
+
+    this.dropdownSettings = {
+      singleSelection: false,
+      text: "Select Campaing",
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      enableSearchFilter: true,
+      enableCheckAll: true,
+      classes: ""
+    };
+    this.dropdownUserSettings = {
+      singleSelection: false,
+      text: "Select user",
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      enableSearchFilter: true,
+      enableCheckAll: true,
+      classes: ""
+    };
+  }
 
   ngOnInit() {
     this.username = localStorage.getItem("user_name")
     this.loginInfo.user_name = this.username;
-    // for (let i = 0; i <= 25; i++) {
-    //   this.customers.push({firstName: `first${i}`, lastName: `last${i}`,
-    //   email: `abc${i}@gmail.com`, address: `000${i} street city, ST`, zipcode: `0000${i}`});
-    // }
-    // this.fetchReportData()
-    this.datapresent=false;
+    this.datapresent = false;
     this.headers = [{ key: '#', value: '#' }, { key: 'lead_id', value: 'Lead Id' },
     { key: 'list_id', value: 'List Id' },
     { key: 'campaign_id', value: 'Campaign Id' },
@@ -53,15 +77,69 @@ export class ReportingComponent implements OnInit {
     { key: 'user', value: 'User' },
     { key: 'comments', value: 'Comments' },
     { key: 'processed', value: 'Processed' },
-    { key: 'user_group', value: 'User Group' },
-    { key: 'term_reason', value: 'Term Reason' },
-    { key: 'alt_dial', value: 'Alt Dial' },
-    { key: 'called_count', value: 'Called Count' }]
+    { key: 'term_reason', value: 'Term Reason' }]
+    this.fetchCampaing();
+    this.fetchUsers();
   }
-  fromDateSelect(event){
+  fetchUsers() {
+    this.userService.fetchUser().subscribe(
+      data => {
+        this.users = data
+        data.forEach((item) => {
+
+          var dropdownListLocal = {
+            id: item.user_id,
+            itemName: item.user
+          }
+
+          this.dropdownUserList.push(dropdownListLocal)
+        })
+        console.log(this.users)
+
+      })
+  }
+  onItemSelect(item: any) {
+  }
+  OnItemDeSelect(item: any) {
+  }
+  onSelectAll(items: any) {
+  }
+  onDeSelectAll(items: any) {
+  }
+
+  
+
+
+
+  onUserItemSelect(item: any) {
+    
+  }
+  OnUserItemDeSelect(item: any) {
+  }
+  onUserSelectAll(items: any) {
+  }
+  onUserDeSelectAll(items: any) {
+  }
+
+
+  fetchCampaing() {
+    this.campaingService.fetchCampaing().subscribe(
+      data => {
+        data.forEach((item) => {
+
+          var dropdownListLocal = {
+            id: item.campaign_id,
+            itemName: item.campaign_name
+          }
+
+          this.dropdownList.push(dropdownListLocal)
+        })
+      })
+  }
+  fromDateSelect(event) {
     console.log(event)
   }
-  toDateSelect(event){
+  toDateSelect(event) {
     console.log(event)
   }
   removeColumn(columnName) {
@@ -78,16 +156,26 @@ export class ReportingComponent implements OnInit {
     })
     console.log(this.headers)
   }
-  fetchResult(){
-    var datefrom=new Date(this.datefrom).getTime()/1000
-    var dateto=new Date(this.dateto).getTime()/1000
-    // console.log(datefrom.getTime()/1000)
-    // console.log(dateto.getTime()/1000)
+  fetchResult(campaingId) {
+    var campaingID = []
+    this.selectedItems.forEach((items => {
+      campaingID.push(items.id)
+    }))
+    var userId = []
+    this.selectedUserItems.forEach((items => {
+      userId.push(items.itemName)
+    }))
+    var requestData = {
+      datefrom: new Date(this.datefrom).getTime() / 1000,
+      dateto: new Date(this.dateto).getTime() / 1000,
+      campaingId: campaingID,
+      userId:userId
+    }
 
-    this.userService.fetchReportDataBetween(datefrom,dateto).subscribe(
+    this.userService.fetchReportDataBetween(requestData).subscribe(
       data => {
-       this.datapresent= data.length==0?false:true
-        this.reportData =data.map((item) => {
+        this.datapresent = data.length == 0 ? false : true
+        this.reportData = data.map((item) => {
           if (item.lead_id == null) { item.lead_id = this.valuenull }
           if (item.list_id == null) { item.list_id = this.valuenull }
           if (item.campaign_id == null) { item.campaign_id = this.valuenull }
@@ -99,16 +187,13 @@ export class ReportingComponent implements OnInit {
           if (item.user == null) { item.user = this.valuenull }
           if (item.comments == null) { item.comments = this.valuenull }
           if (item.processed == null) { item.processed = this.valuenull }
-          if (item.user_group == null) { item.user_group = this.valuenull }
           if (item.term_reason == null) { item.term_reason = this.valuenull }
-          if (item.alt_dial == null) { item.alt_dial = this.valuenull }
-          if (item.called_count == null) { item.called_count = this.valuenull }
           return item
         })
         console.log(this.reportData)
       })
     // var parts = this.datefrom.match(/(\d{2})\/(\d{2})\/(\d{4}) (\d{2}):(\d{2})/);
-// return Date.UTC(+parts[3], parts[2]-1, +parts[1], +parts[4], +parts[5])
+    // return Date.UTC(+parts[3], parts[2]-1, +parts[1], +parts[4], +parts[5])
     // console.log(this.datefrom.getDate())
     // console.log(this.datefrom.getMonth())
     // console.log(this.datefrom.getFullYear())
@@ -117,8 +202,8 @@ export class ReportingComponent implements OnInit {
   fetchReportData() {
     this.userService.fetchReportData().subscribe(
       data => {
-       
-        this.reportData =data.map((item) => {
+
+        this.reportData = data.map((item) => {
           if (item.lead_id == null) { item.lead_id = this.valuenull }
           if (item.list_id == null) { item.list_id = this.valuenull }
           if (item.campaign_id == null) { item.campaign_id = this.valuenull }
