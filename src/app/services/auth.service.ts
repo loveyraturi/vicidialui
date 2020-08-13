f8ff5fb6630365ee5781d811ecc1e707b097449d
root <praveenraturi3@yahoo.com>
2020-08-13 22:42:25 +0530

    changes

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -16,8 +16,7 @@ export class AuthService {
     constructor(private http:HttpClient) {}
 
     validateLogin(ruleConf): Observable<any> {
-        return this.http
-          .post('http://localhost:6001/microapp/goautodial/login', ruleConf).pipe(
+        return this.http.post('http://157.245.109.0:6001/goautodial/login', ruleConf).pipe(
           map(
             res => {
                 return res;
